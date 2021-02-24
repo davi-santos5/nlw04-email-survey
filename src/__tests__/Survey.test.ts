556b76ecf40910c2c314f1113d0eb3cc52eb9366
davi-santos5 <dbs.tj0@gmail.com>
2021-02-24 13:03:49 -0300

    first commit

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+
+import { app } from "../app";
+import createConnection from "../database";
+
+describe("Surveys", () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it("Should be able to create a new survey", async () => {
+    const response = await request(app).post("/surveys").send({
+      title: "Title example",
+      description: "Description example",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+  });
+
+  it("Should get all the existing survey", async () => {
+    await request(app).post("/surveys").send({
+      title: "Other title example",
+      description: "Other description example",
+    });
+
+    const response = await request(app).get("/surveys");
+
+    expect(response.body.length).toBe(2);
+  });
+});
